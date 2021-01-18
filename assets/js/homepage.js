dab4c208d3ec4d72b8dc95b63548a6ab061bfd23
Cooper <cooperflynn@utexas.edu>
2021-01-17 22:45:58 -0600

    add user repo fetch function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
+var getUserRepos = function(user) {
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        var apiUrl = "https://api.github.com/users/" + user + "/repos";
+        fetch(apiUrl).then(function(response) {
+            response.json().then(function(data) {
+                console.log(data);
+                });
+        
+            });
+    });
+
+console.log("outside");
 };
 
-getUserRepos();
+getUserRepos("facebook");
